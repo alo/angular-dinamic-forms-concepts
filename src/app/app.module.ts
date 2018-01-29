f35c302e89ff7e2c1d756ad0683376328f041173
Antonio Genaro <antoniogenaro8@gmail.com>
2018-01-29 19:09:07 +0100

    feat: object form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CustomFormObjectComponent } from './components/custom-form-object/custom-form-object.component';
+import { CustomFormControlComponent } from './components/custom-form-control/custom-form-control.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomFormObjectComponent,
+    CustomFormControlComponent
   ],
   imports: [
     BrowserModule,
